c23762211e04bf4c75e43312f1b34cb3c890098f
Vijay Singh <vijaysingh3225@gmail.com>
2024-12-03 17:13:45 -0500

    application in working state

--- java/src/main/java/com/techelevator/dao/JdbcCardDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcCardDao.java
@@ -7,23 +7,23 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 import java.util.ArrayList;
 import java.util.List;
-
-public class JdbcCardDao implements CardDao{
-    public List<Card> getCardsInCollection(int collectionId) {
-        List<Card> cardsInCollection = new ArrayList<>();
-        String sql = "SELECT card_id, card_name, image_url FROM cards" +
-                "JOIN cards_collections ON cards.card_id = cards_collections.card_id" +
-                "JOIN collections ON cards_collections.collection_id = collections.collection_id" +
-                "WHERE collections.collection.id = ?";
-        try {
-            SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
-            while (results.next()) {
-                Card card = mapRowToCard(results);
-                cardCollections.add(cardCollection);
-            }
-        } catch (CannotGetJdbcConnectionException e) {
-            throw new DaoException("Unable to connect to server or database", e);
-        }
-        return cardsInCollection;
-    }
-}
+//
+//public class JdbcCardDao implements CardDao{
+//    public List<Card> getCardsInCollection(int collectionId) {
+//        List<Card> cardsInCollection = new ArrayList<>();
+//        String sql = "SELECT card_id, card_name, image_url FROM cards" +
+//                "JOIN cards_collections ON cards.card_id = cards_collections.card_id" +
+//                "JOIN collections ON cards_collections.collection_id = collections.collection_id" +
+//                "WHERE collections.collection.id = ?";
+//        try {
+//            SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+//            while (results.next()) {
+//                Card card = mapRowToCard(results);
+//                cardCollections.add(cardCollection);
+//            }
+//        } catch (CannotGetJdbcConnectionException e) {
+//            throw new DaoException("Unable to connect to server or database", e);
+//        }
+//        return cardsInCollection;
+//    }
+//}
