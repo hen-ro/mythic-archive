063fa155a442d3aa332dc2c1de39b40191cac227
Vijay Singh <vijaysingh3225@gmail.com>
2024-12-05 23:08:39 -0500

    image scaling, and sit logo added

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="capstone-app">
     <div id="header">
-      <div class="logo">PokeMans</div>
+      <div class="logo"><img src='/images/MALogo.png' class="website-logo"></div>
       <div id="nav">
       <router-link class="nav-item" v-bind:to="{ name: 'home' }">Home<img src='/images/HomeIcon.png' class="icon"></router-link>
       <router-link class="nav-item" v-bind:to="{ name: 'search' }">Search<img src='/images/SearchIcon.png' class="icon-search"></router-link>
@@ -29,6 +29,7 @@
     justify-content: flex-end;
     margin-right: 40px;
     gap: 40px;
+    align-items: center;
  
 }
 .logo{
@@ -37,7 +38,7 @@
 #header{
   display: flex;
   justify-content: space-between;
-  background-color: #393E41;
+  background-color: #282c2e;
   border-bottom:2px solid #E7E5DF;
   padding-top: 10px;
   padding-bottom: 5px;
@@ -46,12 +47,15 @@
   width:1.8vw;
 }
 .icon{
-  width:25px;
+  width:1.2vw;
   margin-left:10px;
 }
 .icon-search{
-  width:20px;
+  width:1vw;
   margin-left:7px;
+}
+.website-logo{
+ width:15vw;
 }
 
 </style>
