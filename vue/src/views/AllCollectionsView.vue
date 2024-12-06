8db9d6fed53a3d457f3e8bab1fdc14e5d93d2cc7
Dylan Critchfield-Sales <dylancritchfieldsales@gmail.com>
2024-12-06 00:03:23 -0500

    Collection View connection and styling

--- vue/src/views/AllCollectionsView.vue
+++ vue/src/views/AllCollectionsView.vue
@@ -1,72 +1,90 @@
 <template>
-   <div>
-      <h1>Collections</h1>
-      <div class="collection-container">
-         <div class="collection-object" v-for="n in 25" :key="n">
-            <router-link class="router-link" v-bind:to="{ name: 'collection' }">
-               <div class="thumbnail"></div>
-               <div class="collection-data">
-                  <p>Name</p>
-                  <p>Username</p>
-                  <p>Total Cards</p>
-               </div>
-            </router-link>
-         </div>
+  <div>
+    <h1>Collections</h1>
+    <div class="collection-container">
+      <div class="collection-object" v-for="collection in collections" v-bind:key="collection.ownerId">
+        <router-link class="router-link" v-bind:to="{ name: 'collection' }">
+          <img class="thumbnail" :src="collection.thumbnailUrl">
+          <div class="collection-data">
+            <p> {{ collection.collectionName }} </p>
+            <p> Username </p>
+            <p>Total Cards</p>
+          </div>
+        </router-link>
       </div>
-   </div>
+    </div>
+  </div>
 </template>
-  
+
 <script>
-import CollectionService from '../services/CollectionService';
+import CollectionService from "../services/CollectionService";
 
 export default {
 
-   methods: {
-      getAllPublicCollections() {
+   data() {
+    return {
+      collections: [],
+    };
+  },
+
+  methods: {
+    getAllPublicCollections() {
       CollectionService.getAllPublicCollections()
         .then((response) => {
-          console.log (response)
+          this.collections = response.data.map((collection) => ({
+            ownerId: collection.ownerId,
+            thumbnailUrl: collection.thumbnailUrl,
+            collectionName: collection.collectionName,
+            public: collection.public
+          }));
+          console.log(this.collections);
         })
         .catch((error) => {
           console.error("Error fetching data:", error);
         });
-      },
-      },
-      mounted(){
-         this.getAllPublicCollections();
-      }
-   };
+    },
+  },
+  mounted() {
+    this.getAllPublicCollections();
+  },
+};
 </script>
-  
+
 <style>
 .router-link {
-   text-decoration: none;
-   color: white;
+  text-decoration: none;
+  color: white;
 }
 
 .collection-container {
-   display: flex;
-   margin: auto;
-   width: 70%;
-   justify-content: space-evenly;
-   flex-wrap: wrap;
+  display: flex;
+  margin: auto;
+  width: 70%;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
 }
 
 .collection-object {
-   width: 18%;
-   background-color: #377c79;
-   margin-top: 20px;
+  width: 18%;
+  min-width: 200px;
+  background-color: #377c79;
 }
 
 .thumbnail {
-   width: 100%;
-   height: 200px;
-   /* background-color: #8f8787; */
+  width: 100%;
+  height: 200px;
 }
 
 .collection-data {
-   text-align: center;
-   display: flex;
-   flex-direction: column;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  height: 70px;
+  justify-content: space-evenly;
+  background-color: #377c79;
+  padding-bottom: 10px;
+}
+p {
+   margin: 0;
 }
 </style>
