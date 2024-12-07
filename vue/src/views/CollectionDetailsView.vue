4be2da9bdb338b4b227c6fe00d2e957f6764d27c
Dylan Critchfield-Sales <dylancritchfieldsales@gmail.com>
2024-12-07 14:07:51 -0500

    12/7/24

--- vue/src/views/CollectionDetailsView.vue
+++ vue/src/views/CollectionDetailsView.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
+  <div class="collections-details">
     <h1>{{ collectionName }}</h1>
+    <div class="searchBox">
+      <div class="field">
+        <input type="text" id="searchTerm" name="searchTerm" v-model="this.searchTerm" />
+        <button @click="search">Search<img src='/images/SearchIconBlack.png' class="search-icon"></button>
+      </div>
+    </div>
+    <div class="card-container" v-if="cards.length > 0">
+      <div class="card" v-for="card in this.cards" v-bind:key="card.id" v-bind:card="card">
+        <router-link class="router-link" v-bind:to="{ name: 'cardDetails', params: { id: card.id } }">        
+        <img :src="card.imageUrl" class="add-card" />
+      </router-link>
+      </div>
+    </div>
   </div>
 </template>
   
